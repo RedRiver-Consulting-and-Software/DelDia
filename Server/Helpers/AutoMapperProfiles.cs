39b6a6dfbb2d7ec93a6075148950b61c932e2f59
Peter Lindholm <peteralexander112@protonmail.com>
2024-05-21 12:29:29 +0200

    Add AutoMapper Add Swagger

---
+++ Server/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Server.DTO;
+using Server.Models;
+
+namespace Server.Helpers;
+
+public class AutoMapperProfiles : Profile
+{
+    public AutoMapperProfiles()
+    {
+        CreateMap<Board, BoardDTO>();
+        CreateMap<List, ListDTO>();
+        CreateMap<Card, CardDTO>();
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,28 +1,30 @@
 using Microsoft.EntityFrameworkCore;
 using Server.Data;
+using Server.Helpers;
 using Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddScoped<BoardService>();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
 }
 
 app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
